57d49c985bd1cd0ae4cc8825d7866b22ea1b487b
vyctor922 <victoralexandre922@gmail.com>
2022-08-10 15:59:54 -0500

    permitir operação #3

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,41 @@
-number_1 = int(input("Enter your first number: "))
-number_2 = int(input("Enter your second number: "))
+def calculate():
+    operation = input(''' 
+    Please type in the math operation you would like to complete:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    ''')
 
-print(f"{number_1} + {number_2} =", number_1+number_2)
-print(f"{number_1} - {number_2} =", number_1-number_2)
-print(f"{number_1} * {number_2} =", number_1*number_2)
-print(f"{number_1} / {number_2} =", number_1/number_2)
+    number_1 = int(input("Enter your first number: "))
+    number_2 = int(input("Enter your second number: "))
+
+    if operation == '+':
+        print("{number_1} + {number_2} =", number_1+number_2)
+
+    elif operation == '-':
+        print(f"{number_1} - {number_2} =", number_1-number_2)
+
+    elif operation == '*':
+        print(f"{number_1} * {number_2} =", number_1*number_2)
+
+    elif operation == '/':
+        print(f"{number_1} / {number_2} =", number_1/number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+def again():
+    calc_again = input(''' 
+    Do you want to calculate again?
+    Please type Y for YES or N for NO.
+    ''')
+    
+    if calc_again == 'Y':
+        calculate()
+    elif calc_again == 'N':
+        print('See you later.')
+    else:
+        again()
+
+calculate()
