f34edb4e41d7c239901b11d799390e3867a43e8d
vyctor922 <victoralexandre922@gmail.com>
2022-08-09 19:40:58 -0500

    tentativa 2

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,7 @@
-number_1 = int[input("Enter your first number: ")]
-number_2 = int[input("Enter your second number: ")]
+number_1 = int(input("Enter your first number: "))
+number_2 = int(input("Enter your second number: "))
 
-print("number_1 + number_2 =", number_1 + number_2)
+print("{number_1} + {number_2} =", number_1+number_2)
+#print('{} + {} = '.format{number_1, number_2})
+#print(number_1 + number_2)
+
